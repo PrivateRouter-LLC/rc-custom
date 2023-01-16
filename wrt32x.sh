e01f0130971fcbaca445544f2aef85c12d4e58ed
TorGuard Ops <ops@torguard.net>
2023-01-16 06:21:58 +0000

    added more

---
+++ wrt32x.sh
@@ -0,0 +1,217 @@
+#!/usr/bin/env bash
+# /etc/udpate.sh PrivateRouter Update Script
+
+# Verify we are connected to the Internet
+is_connected() {
+    ping -q -c3 1.1.1.1 >/dev/null 2>&1
+    return $?
+}
+
+# Log to the system log and echo if needed
+log_say()
+{
+    echo "${1}"
+    logger "${1}"
+}
+
+log_say "                                                                      "
+log_say " ███████████             ███                         █████            "
+log_say "░░███░░░░░███           ░░░                         ░░███             "
+log_say " ░███    ░███ ████████  ████  █████ █████  ██████   ███████    ██████ "
+log_say " ░██████████ ░░███░░███░░███ ░░███ ░░███  ░░░░░███ ░░░███░    ███░░███"
+log_say " ░███░░░░░░   ░███ ░░░  ░███  ░███  ░███   ███████   ░███    ░███████ "
+log_say " ░███         ░███      ░███  ░░███ ███   ███░░███   ░███ ███░███░░░  "
+log_say " █████        █████     █████  ░░█████   ░░████████  ░░█████ ░░██████ "
+log_say "░░░░░        ░░░░░     ░░░░░    ░░░░░     ░░░░░░░░    ░░░░░   ░░░░░░  "
+log_say "                                                                      "
+log_say "                                                                      "
+log_say " ███████████                        █████                             "
+log_say "░░███░░░░░███                      ░░███                              "
+log_say " ░███    ░███   ██████  █████ ████ ███████    ██████  ████████        "
+log_say " ░██████████   ███░░███░░███ ░███ ░░░███░    ███░░███░░███░░███       "
+log_say " ░███░░░░░███ ░███ ░███ ░███ ░███   ░███    ░███████  ░███ ░░░        "
+log_say " ░███    ░███ ░███ ░███ ░███ ░███   ░███ ███░███░░░   ░███            "
+log_say " █████   █████░░██████  ░░████████  ░░█████ ░░██████  █████           "
+log_say "░░░░░   ░░░░░  ░░░░░░    ░░░░░░░░    ░░░░░   ░░░░░░  ░░░░░            "
+
+# Set our router's dns
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+
+# Check if we are connected, if not, exit
+[ is_connected ] || { log_say "We are not connected to the Internet to run our update script." ; exit 0; }
+
+# Set this to 0 to disable Tankman theme
+TANKMAN_FLAG=1
+
+# This file is our marker to know the first run init script has already ran
+INIT_MARKER="/usr/lib/opkg/info/tankman.list"
+
+# If we are online and our tankman flag is enabled (and we have not already been ran before), do our setup script
+[ ${TANKMAN_FLAG} = "1" ] && [ ! -f "${INIT_MARKER}" ] && {
+        #Install Argon Tankman theme
+        log_say "Installing custom Argon Tankman Theme"
+        opkg install /etc/luci-theme-argon*.ipk
+        opkg install /etc/luci-app-argon*.ipk
+
+        tar xzvf /etc/logo.tar.gz -C /www/luci-static/argon/
+
+        # Delete the background files from /www/luci-static/argon/background
+        # Comment these lines out if you want to avoid bing backgrounds
+        rm -rf /www/luci-static/argon/background/mahsa_amini.png
+        rm -rf /www/luci-static/argon/background/tankman.mp4
+
+        # Marker set so we know theme has been installed
+        log_say "Set our marker file to know our tankman theme install has already ran"
+        touch "${INIT_MARKER}"
+} || {
+        # No need to run setup script
+        log_say "We do not need to run the PrivateRouter Tankman Theme Setup Script or it has already ran"
+}
+
+# Check if we need to update our updater scripts
+log_say "Beginning update-scripts up to date check"
+
+HASH_STORE="/etc/config/.update-scripts"
+TMP_DIR="/tmp/update-scripts"
+GIT_URL="https://github.com/PrivateRouter-LLC/update-scripts"
+UPDATER_LOCATION="/root/update-scripts"
+
+CURRENT_HASH=$(
+    curl \
+        --silent https://api.github.com/repos/PrivateRouter-LLC/update-scripts/commits/main |
+        jq --raw-output '.sha'
+)
+
+if [ -f "${HASH_STORE}" ]; then
+    log_say "Update Script Found ${HASH_STORE}"
+    CHECK_HASH=$(cat ${HASH_STORE})
+    log_say "Update Script Check Hash ${CHECK_HASH}"
+    [[ "${CHECK_HASH}" != "${CURRENT_HASH}" ]] && {
+        log_say "Update Script ${CHECK_HASH} != ${CURRENT_HASH}"
+        UPDATE_NEEDED="1"
+        echo "${CURRENT_HASH}" > "${HASH_STORE}"
+        log_say "Update Script Wrote ${CURRENT_HASH} > ${HASH_STORE}"
+    }
+else
+    log_say "Update Script ${HASH_STORE} did not exist"
+    touch "${HASH_STORE}"
+    echo "${CURRENT_HASH}" > "${HASH_STORE}"
+    log_say "Update Script Wrote ${CURRENT_HASH} > ${HASH_STORE}"
+    UPDATE_NEEDED="1"
+fi
+
+if [[ "${UPDATE_NEEDED}" == "1" || ! -d ${UPDATER_LOCATION} ]]; then
+    log_say "Update Script Update is needed"
+
+    CRONTAB_CONTENT=$(cat "/etc/crontabs/root")
+    [[ "${CRONTAB_CONTENT}" =~ "update-dockerdeploy.sh" ]] && {
+        log_say "Update Script found update-dockerdeploy.sh, removing entry in crontab"
+        sed -i '/update-dockerdeploy.sh/d' /etc/crontabs/root
+    }
+    [[ "${CRONTAB_CONTENT}" =~ "update-docker-compose-templates.sh" ]] && {
+        log_say "Update Script found update-docker-compose-templates.sh, removing entry in crontab"
+        sed -i '/update-docker-compose-templates.sh/d' /etc/crontabs/root
+    }
+    [[ "${CRONTAB_CONTENT}" =~ "update-repo.sh" ]] && {
+        log_say "Update Script found update-repo.sh, removing entry in crontab"
+        sed -i '/update-repo.sh/d' /etc/crontabs/root
+    }
+
+    [ -d "${TMP_DIR}" ] && {
+        log_say "Update Script Cleaning temporary output ${TMP_DIR}"
+        rm -rf "${TMP_DIR}"
+    }
+
+    log_say "Update Script Cloning ${GIT_URL} into ${TMP_DIR}"
+    git clone --depth=1 "${GIT_URL}" "${TMP_DIR}"
+
+    log_say "Update Script Cleaning up .git folder"
+    rm -rf "${TMP_DIR}/.git"
+
+    [ -d "${UPDATER_LOCATION}" ] && { log_say "Update Script Removing old ${UPDATER_LOCATION}"; rm -rf "${UPDATER_LOCATION}"; }
+
+    log_say "Update Script Moving ${TMP_DIR} to ${UPDATER_LOCATION}"
+    mv "${TMP_DIR}" "${UPDATER_LOCATION}"
+
+    [ -f "${UPDATER_LOCATION}/crontabs" ] && {
+        log_say "Update Script Inserting crontabs for updaters and restarting the cron service"
+        cat "${UPDATER_LOCATION}/crontabs" >> /etc/crontabs/root
+        /etc/init.d/cron restart
+    }
+
+    [ -f "${UPDATER_LOCATION}/first-run.sh" ] && {
+        log_say "Running the commands in the first-run.sh script."
+        bash "${UPDATER_LOCATION}/first-run.sh"
+    }
+else
+    log_say "Update Script Update is not needed"
+fi # UPDATE_NEEDED check
+
+# Update and install all of our packages
+log_say "updating all packages!"
+
+opkg install acme attendedsysupgrade-common attr avahi-dbus-daemon base-files busybox ca-bundle certtool cgi-io curl davfs2 dbus ddns-scripts ddns-scripts-cloudflare ddns-scripts-digitalocean
+
+opkg install ddns-scripts-services dnsmasq dropbear firewall fstools fuse3-utils fwtool getrandom git glib2 gnupg hostapd-common ip-full ip6tables ipset iptables iptables-mod-ipopt iw iwinfo jshn
+
+opkg install jsonfilter kernel kmod-bluetooth kmod-btmrvl kmod-cfg80211 kmod-crypto-aead kmod-crypto-ccm kmod-crypto-cmac kmod-crypto-ctr kmod-crypto-ecb kmod-crypto-ecdh kmod-crypto-gcm kmod-crypto-gf128
+
+opkg install kmod-crypto-ghash kmod-crypto-hash kmod-crypto-hmac kmod-crypto-kpp kmod-crypto-lib-blake2s kmod-crypto-lib-chacha20 kmod-crypto-lib-chacha20poly1305 kmod-crypto-lib-curve25519
+
+opkg install kmod-crypto-lib-poly1305 kmod-crypto-manager kmod-crypto-null kmod-crypto-rng kmod-crypto-seqiv kmod-crypto-sha256 kmod-fuse kmod-gpio-button-hotplug kmod-hid kmod-input-core kmod-input-evdev
+
+opkg install kmod-ip6tables kmod-ipt-conntrack kmod-ipt-core kmod-ipt-ipopt kmod-ipt-ipset kmod-ipt-nat kmod-ipt-offload kmod-lib-crc-ccitt kmod-lib-crc16 kmod-mac80211 kmod-mmc kmod-mwifiex-sdio luci-compat luci-lib-ipkg
+
+opkg install kmod-mwlwifi kmod-nf-conntrack kmod-nf-conntrack6 kmod-nf-flow kmod-nf-ipt kmod-nf-ipt6 kmod-nf-nat kmod-nf-reject kmod-nf-reject6 kmod-nfnetlink kmod-nls-base kmod-ppp kmod-pppoe kmod-pppox
+
+opkg install kmod-regmap-core kmod-slhc kmod-tun kmod-udptunnel4 kmod-udptunnel6 kmod-usb-core kmod-wireguard libatomic1 libattr libavahi-client libavahi-dbus-support libblkid1 libbpf0 libbz2-1.0 libc
+
+opkg install libcap libcurl4 libdaemon libdbus libelf1 libev libevdev libevent2-7 libexif libexpat libffi libffmpeg-mini libflac libfuse1 libfuse3-3 libgcc1 libgmp10 libgnutls libhttp-parser
+
+opkg install libid3tag libip4tc2 libip6tc2 libipset13 libiwinfo-data libiwinfo-lua libiwinfo20210430 libjpeg-turbo libjson-c5 liblua5.1.5 liblucihttp-lua liblucihttp0 liblzo2 libmbedtls12 libmnl0
+
+opkg install libmount1 libncurses6 libneon libnettle8 libnftnl11 libnghttp2-14 libnl-tiny1 libogg0 libopenssl-conf libopenssl1.1 libowipcalc libpam libpcre libpopt0 libprotobuf-c libpthread libreadline8
+
+opkg install librt libsmartcols1 libsodium libsqlite3-0 libtasn1 libtirpc libubus-lua libuci-lua libuci20130104 libuclient20201210 libudev-zero liburing libusb-1.0-0 libustream-wolfssl20201210 libuuid1
+
+opkg install libvorbis libxml2 libxtables12 logd lua luci luci-app-ddns luci-app-firewall luci-app-minidlna luci-app-openvpn luci-app-opkg luci-app-samba4 luci-app-statistics luci-mod-dashboard luci-app-vnstat
+
+opkg install luci-app-shadowsocks-libev luci-app-smartdns luci-app-vpn-policy-routing luci-app-vpnbypass luci-app-watchcat luci-app-wireguard luci-base luci-compat luci-i18n-firewall-en
+
+opkg install luci-i18n-wireguard-en luci-lib-base luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system luci-proto-ipv6
+
+opkg install luci-proto-ppp luci-proto-wireguard luci-theme-bootstrap luci-theme-material luci-theme-openwrt-2020 minidlna mount-utils mtd mwifiex-sdio-firmware mwlwifi-firmware-88w8964
+
+opkg install netifd ocserv odhcp6c odhcpd-ipv6only openssh-sftp-client openssh-sftp-server openssl-util openvpn-openssl openwrt-keyring opkg owipcalc ppp ppp-mod-pppoe procd procd-seccomp
+
+opkg install procd-ujail python3-base python3-email python3-light python3-logging python3-openssl python3-pysocks python3-urllib resolveip rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci
+
+opkg install rpcd-mod-rpcsys rpcd-mod-rrdns rsync samba4-libs samba4-server shadowsocks-libev-config shadowsocks-libev-ss-tunnel smartdns socat socksify sshfs terminfo tor ubi-utils
+
+opkg install uboot-envtools ubox ubus ubusd uci uclient-fetch uhttpd uhttpd-mod-ubus urandom-seed urngd usbutils usign vpn-policy-routing vpnbypass vpnc-scripts watchcat wg-installer-client wget-ssl
+
+opkg install wireguard-tools wireless-regdb wpad-basic-wolfssl zlib kmod-usb-storage block-mount samba4-server luci-app-samba4 luci-app-minidlna minidlna kmod-fs-ext4 kmod-fs-exfat e2fsprogs fdisk
+
+echo "Installing TorGuard Wireguard..."
+opkg install /etc/luci-app-tgwireguard_1.0.3-1_all.ipk
+
+## V2RAYA INSTALLER ##
+echo "Installing V2rayA..."
+## download
+
+opkg update; opkg install unzip wget-ssl
+
+## Remove DNSMasq
+
+opkg remove dnsmasq
+
+opkg install dnsmasq-full
+
+opkg install v2raya
+
+opkg install /etc/luci-app-v2raya_6_all.ipk
+
+echo "PrivateRouter update complete!"
+
+exit 0
+
