fa9445d06bdc74c77de1d781b2349f8129c78c77
TorGuard Ops <ops@torguard.net>
2023-01-16 03:26:44 +0000

    Added routers

--- update.sh
+++ update.sh
@@ -5,14 +5,20 @@
 BOARD_CHECK="$(cat /tmp/sysinfo/model)"
 chmod +x /root/rc-custom/*.sh
 
-if [[ "${BOARD_CHECK}" == *"MR8300"* ]]; then
-    bash /root/rc-custom/mr8300.sh
-elif [[ "${BOARD_CHECK}" == *"GL-MT300N-V2"* ]]; then
+if [[ "${BOARD_CHECK}" == *"GL-MT300N-V2"* ]]; then
     bash /root/rc-custom/mt300nv2.sh
-elif [[ "${BOARD_CHECK}" == *"GL-MT300N"* ]]; then # Check for the v2 first because the v1 will match both
-    # TODO
-elif [[ "${BOARD_CHECK}" == *"GL-AR750"* ]]; then
-    bash /root/rc-custom/ar750.sh
+elif [[ "${BOARD_CHECK}" == *"GL-MT300N"* ]]; then
+    bash /root/rc-custom/mt300n.sh
+elif [[ "${BOARD_CHECK}" == *"MR8300"* ]]; then
+    bash /root/rc-custom/mr8300.sh
+elif [[ "${BOARD_CHECK}" == *"GL-AR300M"* ]]; then
+    bash /root/rc-custom/ar300m.sh
+elif [[ "${BOARD_CHECK}" == *"GL-AR750S"* ]]; then
+    bash /root/rc-custom/ar750s.sh
+elif [[ "${BOARD_CHECK}" == *"WRT32X"* ]]; then
+    bash /root/rc-custom/wrt32x.sh
+elif [[ "${BOARD_CHECK}" == *"Archer C7"* ]]; then
+    bash /root/rc-custom/archerc7.sh
 else
     bash /root/rc-custom/generic.sh
 fi
