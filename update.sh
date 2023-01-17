448db6b5a0b0d868932121d03431c286d1273e3e
PrivateRouter <101918538+PrivateRouter@users.noreply.github.com>
2023-01-17 02:58:02 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -19,6 +19,8 @@ elif [[ "${BOARD_CHECK}" == *"GL-AR750S"* ]]; then
     bash /root/rc-custom/ar750s.sh
 elif [[ "${BOARD_CHECK}" == *"GL-MT1300"* ]]; then
     bash /root/rc-custom/mt1300.sh
+elif [[ "${BOARD_CHECK}" == *"GL-E750"* ]]; then
+    bash /root/rc-custom/gle750.sh
 elif [[ "${BOARD_CHECK}" == *"GL-BL1300"* ]]; then
     bash /root/rc-custom/bl1300.sh
 elif [[ "${BOARD_CHECK}" == *"GL-AR750S (NOR)"* ]]; then
