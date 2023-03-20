82a16da65f98cdb9574164734c922695cfc52a00
PrivateRouter <101918538+PrivateRouter@users.noreply.github.com>
2023-03-20 01:22:46 -0400

    Update mt1300.sh
    Added Router App Store

--- mt1300.sh
+++ mt1300.sh
@@ -164,11 +164,9 @@ opkg install openwrt-keyring opkg ppp ppp-mod-pppoe procd px5g-wolfssl kmod-usb-
 opkg install kmod-rt2800-usb rt2800-usb-firmware kmod-cfg80211 kmod-lib80211 kmod-mac80211 kmod-rtl8192cu luci-base luci-ssl luci-mod-admin-full
 opkg install luci-theme-bootstrap kmod-usb-storage kmod-usb-ohci kmod-usb-uhci e2fsprogs fdisk resize2fs htop debootstrap luci-compat luci-lib-ipkg dnsmasq
 
-## V2RAYA INSTALLER ##
-echo "Installing V2rayA..."
+## V2RAYA INSTALLER PREP ##
+log_say "Preparing for V2rayA..."
 ## download
-
-opkg update; opkg install unzip wget-ssl
 
 ## Remove DNSMasq
 
@@ -176,10 +174,11 @@ opkg remove dnsmasq
 
 opkg install dnsmasq-full
 
-opkg install v2raya
+## INSTALL ROUTER APP STORE ##
+log_say "Installing Router App Store..."
+opkg install tgrouterappstore luci-app-shortcutmenu luci-app-poweroff luci-app-wizard tgwireguard
 
-opkg install /etc/luci-app-v2raya_6_all.ipk
+log_say "PrivateRouter update complete!"
 
-echo "PrivateRouter update complete!"
 
 exit 0
