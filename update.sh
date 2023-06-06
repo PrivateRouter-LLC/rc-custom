b4398222ede221bc0f12e4c2e979c43ef9cd2a5a
PrivateRouter <101918538+PrivateRouter@users.noreply.github.com>
2023-06-05 22:22:09 -0400

    Update update.sh
    Added Linksys E8450

--- update.sh
+++ update.sh
@@ -46,6 +46,8 @@ elif [[ "${BOARD_CHECK}" == *"1200ac"* ]]; then
     bash /root/rc-custom/wrt1200ac.sh
 elif [[ "${BOARD_CHECK}" == *"1900ac"* ]]; then
     bash /root/rc-custom/wrt1900ac.sh
+elif [[ "${BOARD_CHECK}" == *"E8450"* ]]; then
+    bash /root/rc-custom/E8450.sh
 elif [[ "${BOARD_CHECK}" == *"Archer C7 v5"* ]]; then
     bash /root/rc-custom/archerc7v5.sh
 else
