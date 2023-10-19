a2a9b9408a19e44ff1d53b132f0c62b651c926c8
Jason Hawks <jason@torguard.net>
2023-10-19 17:04:55 -0400

    LXC Install Fix

--- generic.sh
+++ generic.sh
@@ -240,115 +240,33 @@ fi
 
 # Install LXC on supported routers if it is not installed, we need to make sure it has /opt/docker2 first!
 if ! opkg list-installed | grep -q "^lxc " && [ -d /opt/docker2 ]; then
+    # Get script directory
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
     log_say "Install LXC and related packages"
-    install_packages "lxc lxc-attach lxc-auto lxc-autostart lxc-cgroup lxc-checkconfig lxc-common lxc-config lxc-configs lxc-console lxc-copy lxc-create lxc-destroy lxc-device lxc-execute lxc-freeze lxc-hooks lxc-info lxc-init lxc-ls lxc-monitor lxc-monitord lxc-snapshot lxc-start lxc-stop lxc-templates lxc-top lxc-unfreeze lxc-unprivileged lxc-unshare lxc-user-nic lxc-usernsexec lxc-wait liblxc luci-app-lxc luci-i18n-lxc-en rpcd-mod-lxc"
-    mkdir -p /opt/docker2/compose/lxc
-    rm /etc/lxc/default.conf
-    rm /etc/lxc/lxc.conf
-    touch /etc/lxc/default.conf
-    touch /etc/lxc/lxc.conf
-    echo "lxc.lxcpath = /opt/docker2/compose/lxc" > /etc/lxc/lxc.conf
-    cat > /etc/lxc/default.conf <<EOL
-#lxc.net.0.type = empty
-lxc.net.0.type = veth
-lxc.net.0.link = br-lan
-lxc.net.0.flags = up
-#lxc.net.0.hwaddr = 00:FF:DD:BB:CC:01
-EOL
+    opkg install lxc lxc-attach lxc-auto lxc-autostart lxc-cgroup lxc-checkconfig lxc-common lxc-config lxc-configs lxc-console lxc-copy lxc-create lxc-destroy lxc-device lxc-execute lxc-freeze lxc-hooks lxc-info lxc-init lxc-ls lxc-monitor lxc-monitord lxc-snapshot lxc-start lxc-stop lxc-templates lxc-top lxc-unfreeze lxc-unprivileged lxc-unshare lxc-user-nic lxc-usernsexec lxc-wait liblxc luci-app-lxc luci-i18n-lxc-en rpcd-mod-lxc
 
-    rm /etc/init.d/lxc-auto
-    touch /etc/init.d/lxc-auto
-    chmod +x /etc/init.d/lxc-auto
-    cat > /etc/init.d/lxc-auto <<EOL
-#!/bin/bash /etc/rc.common
+    log_say "Create LXC directories and files"
+    [ ! -d /opt/docker2/compose/lxc ] && mkdir -p /opt/docker2/compose/lxc
 
-. /lib/functions.sh
-
-START=99
-STOP=00
-
-run_command() {
-	local command="$1"
-	$command
-}
-
-start_container() {
-    local cfg="$1"
-    local name
-
-    config_get name "$cfg" name
-    config_list_foreach "$cfg" command run_command
-
-    if [ -n "$name" ]; then
-        local config_path="/opt/docker2/compose/lxc/$name/config"
-
-        # Change permissions so that the script can write to the file
-        chmod 664 "$config_path" || echo "Failed to set permissions on $config_path" >> /etc/lxc/error.log
-
-        # Generate a random MAC address
-        local MAC=$(od -An -N6 -tx1 /dev/urandom | sed -e 's/  */:/g' -e 's/^://')
-
-        # Debugging: log the MAC address generation
-        echo "Debug: Generated MAC $MAC for $name" >> /etc/lxc/error.log
-
-        # Remove existing MAC address setting if it exists
-        sed -i "/^lxc.net.0.hwaddr/d" "$config_path"
-
-        # Add new MAC address setting
-        echo "lxc.net.0.hwaddr = $MAC" >> "$config_path" || echo "Failed to write MAC address to $config_path" >> /etc/lxc/error.log
-
-        # Start the container
-        /usr/bin/lxc-start -n "$name"
+    if [ -f "${SCRIPT_DIR}/lxc/default.conf" ]; then
+        log_say "Copying default.conf to /etc/lxc"
+        [ -f /etc/lxc/default.conf ] && mv /etc/lxc/default.conf /etc/lxc/default.conf.bak
+        cat "${SCRIPT_DIR}/lxc/default.conf" /etc/lxc/default.conf
     fi
-}
 
-max_timeout=0
+    if [ -f "${SCRIPT_DIR}/lxc/lxc.conf" ]; then
+        log_say "Copying lxc.conf to /etc/lxc"
+        [ -f /etc/lxc/lxc.conf ] && mv /etc/lxc/lxc.conf /etc/lxc/lxc.conf.bak
+        cp "${SCRIPT_DIR}/lxc/lxc.conf" /etc/lxc/lxc.conf
+    fi
 
-stop_container() {
-	local cfg="$1"
-	local name timeout
-
-	config_get name "$cfg" name
-	config_get timeout "$cfg" timeout 300
-
-	if [ "$max_timeout" -lt "$timeout" ]; then
-		max_timeout=$timeout
-	fi
-
-	if [ -n "$name" ]; then
-		/usr/bin/lxc-stop -n "$name" -t $timeout &
-	fi
-}
-
-start() {
-	config_load lxc-auto
-	config_foreach start_container container
-}
-
-stop() {
-	config_load lxc-auto
-	config_foreach stop_container container
-	# ensure e.g. shutdown doesn't occur before maximum timeout on
-	# containers that are shutting down
-	if [ $max_timeout -gt 0 ]; then
-		sleep $max_timeout
-	fi
-}
-
-#Export systemd cgroups
-boot() {
-	if [ ! -d /sys/fs/cgroup/systemd ]; then
-		mkdir -p /sys/fs/cgroup/systemd
-		mount -t cgroup -o rw,nosuid,nodev,noexec,relatime,none,name=systemd cgroup /sys/fs/cgroup/systemd
-	fi
-
-	if [ ! -d /run ]; then
-		ln -s /var/run /run
-	fi
-
-	start
-}
-EOL
+    if [ -f "${SCRIPT_DIR}/lxc/lxc-auto" ]; then
+        log_say "Copying lxc-auto to /etc/init.d"
+        [ -f /etc/init.d/lxc-auto ] && mv /etc/init.d/lxc-auto /etc/init.d/lxc-auto.bak
+        cp "${SCRIPT_DIR}/lxc/lxc-auto" /etc/init.d/lxc-auto
+        chmod +x /etc/init.d/lxc-auto
+    fi
 fi
 # End LXC install
 
